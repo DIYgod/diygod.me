8090060f76fa950272870de68a437cdc3982137b
DIYgod <diy.d.god@gmail.com>
2020-02-08 03:43:14 +0800

    feat: header

--- themes/sagiri/src/mix.js
+++ themes/sagiri/src/mix.js
@@ -1,10 +1,10 @@
 // 顶部头像动画
-$('.site-master-avatar').on('mouseover', function () {
-  this.classList.add('animated', 'tada');
-});
-$('.site-master-avatar').on('mouseout', function () {
-  this.classList.remove('animated', 'tada');
-});
+// $('.site-master-avatar').on('mouseover', function () {
+//   this.classList.add('animated', 'tada');
+// });
+// $('.site-master-avatar').on('mouseout', function () {
+//   this.classList.remove('animated', 'tada');
+// });
 
 // subtitle
 if (navigator.userAgent.match(/mobile/i)) {
