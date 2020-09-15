6b55a1c18aeba035addebf7f0e7116ef59cd39c8
DIYgod <diy.d.god@gmail.com>
2020-09-16 03:39:30 +0800

    feat: disable fixed aplayer in mobile

--- themes/sagiri/src/aplayer.js
+++ themes/sagiri/src/aplayer.js
@@ -1,14 +1,16 @@
-const apFixed = new APlayer({
-  element: document.getElementById('aplayer-fixed'),
-  mutex: true,
-  theme: '#97dffd',
-  order: 'random',
-  lrcType: 3,
-  fixed: true,
-});
-$.ajax({
-  url: 'https://api.i-meto.com/meting/api?server=netease&type=playlist&id=35798529',
-  success: function (list) {
-    apFixed.list.add(list);
-  }
-});
+if (!NexT.utils.isMobile()) {
+  const apFixed = new APlayer({
+    element: document.getElementById('aplayer-fixed'),
+    mutex: true,
+    theme: '#97dffd',
+    order: 'random',
+    lrcType: 3,
+    fixed: true,
+  });
+  $.ajax({
+    url: 'https://api.i-meto.com/meting/api?server=netease&type=playlist&id=35798529',
+    success: function (list) {
+      apFixed.list.add(list);
+    }
+  });
+}
