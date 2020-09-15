74054fc6449842d1c0c5af67aa18b34444d48291
DIYgod <diy.d.god@gmail.com>
2020-09-15 19:31:25 +0800

    feat: use webp and lazyload

---
+++ themes/sagiri/scripts/img.js
@@ -0,0 +1,5 @@
+hexo.extend.filter.register('after_render:html', (htmlContent) => {
+  return htmlContent.replace(/<img(.*?)src="(.*?)"(.*?)>/gi, function (str, p1, p2) {
+      return `<picture><source srcset="${p2.replace(/\.(jpg|jpeg|png)/gi, '.webp')}" type="image/webp">${str.replace('<img', '<img loading="lazy"')}</picture>`;
+  });
+});
